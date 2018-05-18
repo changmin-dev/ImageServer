32c0110b0c4674ea41964d1dc6d5861af39f671d
Changmin Kim <Changmin043@gmail.com>
2018-05-18 13:13:41 +0900

    중복 되는 메서드 제거

--- src/main/java/phi/service/GIFProcessService.java
+++ src/main/java/phi/service/GIFProcessService.java
@@ -31,31 +31,7 @@ public class GIFProcessService {
                 .toAbsolutePath()
                 .normalize();
     }
-    //과연 리스트로 들어오는게 순서대로 인지 확인 필요
-    public void generateAnimatedGif(List<String> inputFileNames, String outputFileName){
-        try {
-            //일단 imageInfo로 가지고 오는 정보는 동일하다고 가정 - 추후 바꿔야 할꺼 같은데 일단 기본 기능 구현에 집중
-            //resolve 하기 fileStoragePath.resolve();
-            ImageInfo imageInfo = new ImageInfo(inputFileNames.get(0));
 
-            //파일 정보, 이미지 정보가 다른 경우?
-            MagickImage[] images = new MagickImage[inputFileNames.size()];
-            ImageInfo[] imageInfos = new ImageInfo[inputFileNames.size()];
-            for(int i = 0; i < inputFileNames.size(); ++i){
-                imageInfo = new ImageInfo(inputFileNames.get(i));
-                images[i] = new MagickImage(imageInfo);
-            }
-
-            MagickImage animatedGifImage = new MagickImage(images);
-            String outputFileDir = fileStoragePath.resolve(outputFileName).toString();
-            animatedGifImage.setFileName(outputFileDir);
-            animatedGifImage.writeImage(imageInfo);
-
-        } catch (MagickException e) {
-            e.printStackTrace();
-        }
-
-    }
     //딜레이의 값? Int면 되나? 코드 중복 제거 필요
     public void generateAnimatedGif(List<String> inputFileNames, String outputFileName, int delay){
         try {
